ad5d5d1b9273be2272fa5f838fe2fd9e71999dd0
ivanap <ivanapantazi@redcross.org.uk>
2021-03-15 16:00:32 +0000

    Split out buttons and links content into pages

---
+++ src/views/ui-components/links/index.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+import LinkButton from "../../../components/link-button";
+import Link from "../../../components/link";
+
+const LinksPage = () => {
+  return (
+    <React.Fragment>
+      <div className="inner">
+        <div className="fold__header">
+          <div className="fold__header__block">
+            <h2 className="fold__title margin-reset">Links </h2>
+          </div>
+        </div>
+        <div className="fold__body">
+          <div className="container-full">
+            <p>This is a link and this is some information</p>
+          </div>
+        </div>
+      </div>
+      <div className="tab__wrap">
+        <Tabs>
+          <TabList>
+            <Tab>Example</Tab>
+            <Tab>Code</Tab>
+          </TabList>
+
+          <TabPanel>
+            <div className="row flex">
+              <div className="buttons-page">
+                <Link
+                  href="#link"
+                  icon="chevron-right"
+                  class="link--with-icon"
+                  name="Link with icon"
+                />
+
+                <Link
+                  href="#link"
+                  icon="chevron-right"
+                  class="link--with-icon-text"
+                  name="Link with icon text"
+                />
+                <Link
+                  href="#link"
+                  icon="chevron-right"
+                  class="link--with-icon-inner"
+                  name="Link with icon inner"
+                />
+
+                <Link
+                  href="#link"
+                  class="link-underline"
+                  name="Link underline"
+                />
+                <Link
+                  href="#link"
+                  class="link--external"
+                  name="Link external"
+                />
+                <Link href="#link" class="link--table" name="Link table" />
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <p>Any content 2</p>
+          </TabPanel>
+        </Tabs>
+
+        <div className="fold__header">
+          <div className="fold__header__block">
+            <h2 className="fold__title margin-reset">Button styled links</h2>
+          </div>
+        </div>
+        <div className="fold__body">
+          <div className="container-full">
+            <p>This is a button styled link and this is some information</p>
+          </div>
+        </div>
+      </div>
+      <div className="tab__wrap">
+        <Tabs>
+          <TabList>
+            <Tab>Example</Tab>
+            <Tab>Code</Tab>
+          </TabList>
+
+          <TabPanel>
+            <div className="buttons-page">
+              <h3>Primary</h3>
+              <LinkButton
+                href="#link"
+                icon="pencil"
+                classSize="small"
+                classType="primary-filled"
+                name="primary filled button styled link"
+              />
+              <LinkButton
+                href="#link"
+                classSize="small"
+                classType="primary-bounded"
+                name="primary bounded button styled link"
+              />
+              <LinkButton
+                href="#link"
+                classSize="small"
+                classType="primary-filled disabled"
+                name="primary filled disabled button styled link"
+              />
+              <LinkButton
+                href="#link"
+                classSize="small"
+                classType="primary-bounded disabled"
+                name="primary bounded disabled button styled link"
+              />
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <p>Any content 2</p>
+          </TabPanel>
+        </Tabs>
+      </div>
+    </React.Fragment>
+  );
+};
+export default LinksPage;
