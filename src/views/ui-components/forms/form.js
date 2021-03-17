55100182475be43dcdeea0aab06aac1742be0adf
ivanap <ivanapantazi@redcross.org.uk>
2021-03-17 17:36:02 +0000

    Handle text input change

--- src/views/ui-components/forms/form.js
+++ src/views/ui-components/forms/form.js
@@ -55,190 +55,226 @@ class Form extends React.Component {
     });
   }
 
+  validateForm() {
+    let fields = this.state.fields;
+    let currentState = this.state.active;
+    let errors = {};
+    let formIsValid = true;
+
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "*Please fill in the field.";
+    }
+
+    if (!fields["textarea"]) {
+      formIsValid = false;
+      errors["textarea"] = "*Please fill in the field.";
+    }
+
+    this.setState({
+      errors: errors,
+      active: currentState,
+    });
+    return formIsValid;
+  }
+
   handleSubmit(event) {
-    alert(
-      "A text input was submitted: " +
-        this.state.fields.name +
-        "A textarea was submitted: " +
-        this.state.fields.textarea
-    );
     event.preventDefault();
+    if (this.validateForm()) {
+      this.setState({ submitted: !this.state.submitted });
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="inner">
-          <div className="fold__header">
-            <div className="fold__header__block">
-              <h2 className="fold__title margin-reset">Input type text</h2>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="inner">
+            <div className="fold__header">
+              <div className="fold__header__block">
+                <h2 className="fold__title margin-reset">Input type text</h2>
+              </div>
+            </div>
+            <div className="fold__body">
+              <div className="container-full">
+                <p>This is a input type and this is some information</p>
+              </div>
             </div>
           </div>
-          <div className="fold__body">
-            <div className="container-full">
-              <p>This is a input type and this is some information</p>
-            </div>
-          </div>
-        </div>
-        <div className="tab__wrap">
-          <Tabs>
-            <TabList>
-              <Tab>Example</Tab>
-              <Tab>Code</Tab>
-            </TabList>
+          <div className="tab__wrap">
+            <Tabs>
+              <TabList>
+                <Tab>Example</Tab>
+                <Tab>Code</Tab>
+              </TabList>
 
-            <TabPanel>
-              <FormInput
-                label="label"
-                type="text"
-                name="name"
-                id="id"
-                value={this.state.fields.name}
-                onChange={this.handleChange}
-                autoFocus
-              />
-            </TabPanel>
-            <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
-          </Tabs>
-        </div>
-        <div className="inner">
-          <div className="fold__header">
-            <div className="fold__header__block">
-              <h2 className="fold__title margin-reset">Input textarea</h2>
+              <TabPanel>
+                <FormInput
+                  label="label"
+                  type="text"
+                  name="name"
+                  id="id"
+                  value={this.state.fields.name}
+                  onChange={this.handleChange}
+                  autoFocus
+                >
+                  <p className="form__error">{this.state.errors.name}</p>
+                </FormInput>
+              </TabPanel>
+              <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
+            </Tabs>
+          </div>
+          <div className="inner">
+            <div className="fold__header">
+              <div className="fold__header__block">
+                <h2 className="fold__title margin-reset">Input textarea</h2>
+              </div>
+            </div>
+            <div className="fold__body">
+              <div className="container-full">
+                <p>This is a input type and this is some information</p>
+              </div>
             </div>
           </div>
-          <div className="fold__body">
-            <div className="container-full">
-              <p>This is a input type and this is some information</p>
-            </div>
-          </div>
-        </div>
-        <div className="tab__wrap">
-          <Tabs>
-            <TabList>
-              <Tab>Example</Tab>
-              <Tab>Code</Tab>
-            </TabList>
+          <div className="tab__wrap">
+            <Tabs>
+              <TabList>
+                <Tab>Example</Tab>
+                <Tab>Code</Tab>
+              </TabList>
 
-            <TabPanel>
-              <FormTextarea
-                label="label"
-                type="text"
-                name="textarea"
-                id="id"
-                value={this.state.fields.textarea}
-                onChange={this.handleChange}
-              />
-            </TabPanel>
-            <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
-          </Tabs>
-        </div>
-        <div className="inner">
-          <div className="fold__header">
-            <div className="fold__header__block">
-              <h2 className="fold__title margin-reset">Select option</h2>
+              <TabPanel>
+                <FormTextarea
+                  label="label"
+                  type="text"
+                  name="textarea"
+                  id="id"
+                  value={this.state.fields.textarea}
+                  onChange={this.handleChange}
+                >
+                  <p className="form__error">{this.state.errors.textarea}</p>
+                </FormTextarea>
+              </TabPanel>
+              <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
+            </Tabs>
+          </div>
+          <div className="inner">
+            <div className="fold__header">
+              <div className="fold__header__block">
+                <h2 className="fold__title margin-reset">Select option</h2>
+              </div>
+            </div>
+            <div className="fold__body">
+              <div className="container-full">
+                <p>This is a Select option and this is some information</p>
+              </div>
             </div>
           </div>
-          <div className="fold__body">
-            <div className="container-full">
-              <p>This is a Select option and this is some information</p>
-            </div>
-          </div>
-        </div>
-        <div className="tab__wrap">
-          <Tabs>
-            <TabList>
-              <Tab>Example</Tab>
-              <Tab>Code</Tab>
-            </TabList>
+          <div className="tab__wrap">
+            <Tabs>
+              <TabList>
+                <Tab>Example</Tab>
+                <Tab>Code</Tab>
+              </TabList>
 
-            <TabPanel>
-              <FormSelect
-                name="Select form"
-                onChange={this.handleChange}
-                options={this.state.selectList}
-                value={this.state.selectList.value}
-              />
-            </TabPanel>
-            <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
-          </Tabs>
-        </div>
-        <div className="inner">
-          <div className="fold__header">
-            <div className="fold__header__block">
-              <h2 className="fold__title margin-reset">Input checkbox</h2>
+              <TabPanel>
+                <FormSelect
+                  name="Select form"
+                  onChange={this.handleChange}
+                  options={this.state.selectList}
+                  value={this.state.selectList.value}
+                />
+              </TabPanel>
+              <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
+            </Tabs>
+          </div>
+          <div className="inner">
+            <div className="fold__header">
+              <div className="fold__header__block">
+                <h2 className="fold__title margin-reset">Input checkbox</h2>
+              </div>
+            </div>
+            <div className="fold__body">
+              <div className="container-full">
+                <p>This is a input checkbox and this is some information</p>
+              </div>
             </div>
           </div>
-          <div className="fold__body">
-            <div className="container-full">
-              <p>This is a input checkbox and this is some information</p>
-            </div>
-          </div>
-        </div>
-        <div className="tab__wrap">
-          <Tabs>
-            <TabList>
-              <Tab>Example</Tab>
-              <Tab>Code</Tab>
-            </TabList>
+          <div className="tab__wrap">
+            <Tabs>
+              <TabList>
+                <Tab>Example</Tab>
+                <Tab>Code</Tab>
+              </TabList>
 
-            <TabPanel>
-              <FormCheckbox
-                name="Checkbox 1"
-                id="id1"
-                onClick={this.handleCheckbox}
-                checked={this.state.checked}
-                type="checkbox"
-                description="Checkbox 1"
-                value={this.state.value}
-                onChange={this.handleCheckbox}
-              />
-            </TabPanel>
-            <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
-          </Tabs>
-        </div>
-        <div className="inner">
-          <div className="fold__header">
-            <div className="fold__header__block">
-              <h2 className="fold__title margin-reset">Input radio button</h2>
+              <TabPanel>
+                <FormCheckbox
+                  name="Checkbox 1"
+                  id="id1"
+                  onClick={this.handleCheckbox}
+                  checked={this.state.checked}
+                  type="checkbox"
+                  description="Checkbox 1"
+                  value={this.state.value}
+                  onChange={this.handleCheckbox}
+                />
+              </TabPanel>
+              <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
+            </Tabs>
+          </div>
+          <div className="inner">
+            <div className="fold__header">
+              <div className="fold__header__block">
+                <h2 className="fold__title margin-reset">Input radio button</h2>
+              </div>
+            </div>
+            <div className="fold__body">
+              <div className="container-full">
+                <p>This is a input radio button and this is some information</p>
+              </div>
             </div>
           </div>
-          <div className="fold__body">
-            <div className="container-full">
-              <p>This is a input radio button and this is some information</p>
-            </div>
-          </div>
-        </div>
-        <div className="tab__wrap">
-          <Tabs>
-            <TabList>
-              <Tab>Example</Tab>
-              <Tab>Code</Tab>
-            </TabList>
+          <div className="tab__wrap">
+            <Tabs>
+              <TabList>
+                <Tab>Example</Tab>
+                <Tab>Code</Tab>
+              </TabList>
 
-            <TabPanel>
-              <FormRadio
-                label="Radio option 1"
-                name="group"
-                id="id1"
-                checked={this.state.selectedOption === this.state.value}
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-              <FormRadio
-                label="Radio option 2"
-                name="group"
-                id="id2"
-                checked={this.state.selectedOption === this.state.value}
-                value={this.state.value}
-                onChange={this.handleChange}
-              />
-            </TabPanel>
-            <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
-          </Tabs>
+              <TabPanel>
+                <FormRadio
+                  label="Radio option 1"
+                  name="group"
+                  id="id1"
+                  checked={this.state.selectedOption === this.state.value}
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+                <FormRadio
+                  label="Radio option 2"
+                  name="group"
+                  id="id2"
+                  checked={this.state.selectedOption === this.state.value}
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+              </TabPanel>
+              <TabPanel>{/* <Code source={`${htmlString}`} /> */}</TabPanel>
+            </Tabs>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+        <div
+          className={
+            this.state.submitted ? "form-message show" : "form-message"
+          }
+        >
+          <p>You have submitted:</p>
+          <span>{this.state.fields.name}</span>
+          <br />
+          <span>{this.state.fields.textarea}</span>
         </div>
-        <input type="submit" value="Submit" />
-      </form>
+      </div>
     );
   }
 }
