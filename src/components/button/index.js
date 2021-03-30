1123e37f83aede2bdb2f337041a78b2383e8f78f
ivanap <ivanapantazi@redcross.org.uk>
2021-03-30 14:19:14 +0100

    Updated markup

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -11,29 +11,4 @@ function Button({ children, ...props }) {
     </button>
   );
 }
-
-// function Button(props) {
-//   const inputType = props.type;
-
-//   if (inputType) {
-//     return (
-//       <button
-//         tabIndex="0"
-//         className={`button button--${props.classSize} button--${props.classType}`}
-//         type={props.type}
-//       >
-//         <span>{props.name}</span>
-//       </button>
-//     );
-//   }
-//   return (
-//     <button
-//       tabIndex="0"
-//       className={`button button--${props.classSize} button--${props.classType}`}
-//     >
-//       <span>{props.name}</span>
-//     </button>
-//   );
-// }
-
 export default Button;
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,53 +1,74 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Progress from "./../progress-labeled/";
+import Button from "./../button/";
 
-const Card = () => {
+const Card = ({
+  linkTo,
+  operationName,
+  emergencyDeployments,
+  emergencyDeploymentsErus,
+  beneficiaries,
+  lastUpdate,
+  requested,
+  operationPopulation,
+  operationCardUnits,
+  operationCardFunding,
+  operationCardDeployed,
+  operationCardFundingCoverage,
+  requestedPercentage,
+}) => {
   return (
-    <div className="key-emergencies-item col col-6-sm col-4-mid" key="key">
-      <Link to="/emergencies/">
+    <div className="key-emergencies-item col col-6-sm col-5-mid" key="key">
+      <a href={linkTo}>
         <div className="card_box card_box_left card_box_title">
           <div className="row flex">
             <div className="card__title__wrap col col-7 col-8-mid">
-              <h2 className="card__title">hello</h2>
+              <h2 className="card__title">{operationName}</h2>
+            </div>
+            <div className="card__title__wrap col col-5 col-4-mid">
+              <Button size="small" type="primary-filled">
+                follow
+              </Button>
             </div>
           </div>
-          <small className="last_updated">dgdgd &nbsp;</small>
+          <small className="last_updated">
+            Last updated: &nbsp;{lastUpdate}
+          </small>
         </div>
 
         <div className="card_box_container card_box_container--op">
           <div className="card_box card_box_left card_box--op">
-            <div className="card_box_no">bvbv</div>
+            <div className="card_box_no">{beneficiaries}</div>
             <span className="affected_population_icon"></span>
-            <small className="heading-tiny">dgggd</small>
+            <small className="heading-tiny">{operationPopulation}</small>
           </div>
           <div className="card_box card_box_left card_box--op">
             <span className="affected_population_icon"></span>
-            <div className="card_box_no">vbv</div>
-            <small className="heading-tiny">dgdgd</small>
+            <div className="card_box_no">{emergencyDeploymentsErus}</div>
+            <small className="heading-tiny">{operationCardUnits}</small>
           </div>
         </div>
 
         <div className="card_box_container card_box_container--op">
           <div className="card_box card_box_left card_box--op">
-            <div className="card_box_no">vbvb</div>
-            <small className="heading-tiny">vbvbvb</small>
+            <div className="card_box_no">{requested}</div>
+            <small className="heading-tiny">{operationCardFunding}</small>
           </div>
           <div className="card_box card_box_left card_box--op">
             <span className="deployed_personnel_icon"></span>
-            <div className="card_box_no">vbvb</div>
-            <small className="heading-tiny">vbvbb</small>
+            <div className="card_box_no">{emergencyDeployments}</div>
+            <small className="heading-tiny">{operationCardDeployed}</small>
           </div>
         </div>
 
         <div className="card_box_footer">
           <Progress value={40} max={100} />
           <div className="card_box_full card_box_container card_box_container--op">
-            <div className="heading-tiny">vbvbv</div>
-            <div className="card_box_fc">bvbvcbcv</div>
+            <div className="heading-tiny">{operationCardFundingCoverage}</div>
+            <div className="card_box_fc">{requestedPercentage}</div>
           </div>
         </div>
-      </Link>
+      </a>
     </div>
   );
 };
