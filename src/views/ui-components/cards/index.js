408a918dda331324396108e35e2ba31887a92c39
JulioP <60132799+JulioPeral@users.noreply.github.com>
2021-04-21 23:09:18 +0100

    IGUL-35 Added content structure to Cards page

--- src/views/ui-components/cards/index.js
+++ src/views/ui-components/cards/index.js
@@ -29,13 +29,12 @@ const CardsPage = () => {
     <React.Fragment>
       <div className="inner">
         <div className="fold__header">
+          <h1>Cards</h1>
+          <p>Placeholder text. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede. </p>
+        </div>
+        <div className="fold__header">
           <div className="fold__header__block">
             <h2 className="fold__title margin-reset">Cards</h2>
-          </div>
-        </div>
-        <div className="fold__body">
-          <div className="container-full">
-            <p>This is a card and this is some information</p>
           </div>
         </div>
       </div>
@@ -70,6 +69,10 @@ const CardsPage = () => {
             <Code source={`${htmlString}`} />
           </TabPanel>
         </Tabs>
+      </div>
+      <div className="container-full">
+        <h2 className="fold__title margin-reset">Comments</h2>
+        <br /><p>Placeholder text</p>
       </div>
     </React.Fragment>
   );
