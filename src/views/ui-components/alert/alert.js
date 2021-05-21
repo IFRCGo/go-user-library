627808575a4e159429afb720ed7eea61b150eb5d
Oliver Westbury <owestbury@gmail.com>
2021-05-21 11:46:56 +0100

    updated alert styles and added react icons

--- src/views/ui-components/alert/alert.js
+++ src/views/ui-components/alert/alert.js
@@ -4,6 +4,11 @@ import ReactDOMServer from "react-dom/server";
 import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
 import c from 'classnames';
 
+import {IoWarningOutline} from 'react-icons/io5';
+import {AiOutlineInfoCircle} from 'react-icons/ai';
+import {FiAlertTriangle} from 'react-icons/fi';
+import {CgCheckO} from 'react-icons/cg';
+
 import LanguageContext from '../../../languageContext';
 import Translate from '../../../components/Translate';
 
@@ -55,6 +60,7 @@ class Alert extends React.Component {
         });
         const {strings} = this.context;
 
+
         return (
             <div>
                 <div className="tab__wrap">
@@ -66,14 +72,25 @@ class Alert extends React.Component {
 
                         <TabPanel>
                             {this.state.isHidden && (
-                                <div className={cl} role='alert'>
-                                    <button className='alert__button-dismiss' title={strings.alertDismissTitle}
-                                            onClick={this.onDismissAlert}>
-                                            <span>
-                                              <Translate stringId='alertDismiss'/>
-                                            </span>
-                                    </button>
-                                    {this.props.text}
+                                <div className={cl + ' ' + this.props.option + ' ' + this.props.type} role='alert'>
+                                        <button className='alert__button-dismiss' title={strings.alertDismissTitle}
+                                                onClick={this.onDismissAlert}>
+                                                <span>
+                                                  <Translate stringId='alertDismiss'/>
+                                                </span>
+                                        </button>
+
+                                        <span className='icon'>
+                                            {this.props.type === "warning" ?
+                                                <IoWarningOutline size="24px" className={this.props.type}/> : null}
+                                            {this.props.type === "danger" ?
+                                                <FiAlertTriangle size="24px" className={this.props.type}/> : null}
+                                            {this.props.type === "info" ?
+                                                <AiOutlineInfoCircle size="24px" className={this.props.type}/> : null}
+                                            {this.props.type === "success" ?
+                                                <CgCheckO size="24px" className={this.props.type}/> : null}
+                                        </span>
+                                        <span className='text'>{this.props.text}</span>
                                 </div>
                             )}
                         </TabPanel>
@@ -92,6 +109,7 @@ export default Alert;
 Alert.contextType = LanguageContext;
 
 Alert.propTypes = {
+    option: T.string,
     type: T.string,
     autoDismiss: T.string,
     popover: T.bool,
--- src/views/ui-components/alert/index.js
+++ src/views/ui-components/alert/index.js
@@ -7,7 +7,7 @@ const AlertPage = () => {
             <div className="inner">
                 <div className="fold__header">
                     <div className="fold__header__block">
-                        <h2 className="fold__title margin-reset">Alert</h2>
+                        <h2 className="fold__title margin-reset">ALERT BOX</h2>
                     </div>
                 </div>
                 <div className="fold__body">
@@ -18,14 +18,46 @@ const AlertPage = () => {
                             Info,
                             danger
                         </p>
+                        <p>https://react-icons.github.io/react-icons</p>
                     </div>
                 </div>
             </div>
 
+            <h2>Primary (Option 1)</h2>
+            <div className="container-full">
+                <b>Warning alert</b>
+                <Alert
+                    id='1'
+                    option='primary'
+                    type='warning'
+                    text='Warning Alert'
+                    autoDismiss='0'
+                />
+                <b>Success alert</b>
+                <Alert
+                    id='2'
+                    option='primary'
+                    type='success'
+                    text='Success Alert'
+                    autoDismiss='0'
+                />
+                <b>Info alert</b>
+                <Alert
+                    id='3'
+                    option='primary'
+                    type='info'
+                    text='Info Alert'
+                />
+            </div>
+            <hr/>
+
+            <h2>Secondary (Option 2)</h2>
+
             <div className="container-full">
                 <b>Success alert</b>
                 <Alert
-                    id='1'
+                    id='4'
+                    option='secondary'
                     type='success'
                     text='Success Alert'
                     autoDismiss='0'
@@ -35,7 +67,8 @@ const AlertPage = () => {
             <div className="container-full">
                 <b>Warning alert</b>
                 <Alert
-                    id='2'
+                    id='5'
+                    option='secondary'
                     type='warning'
                     text='Warning Alert'
                     autoDismiss='0'
@@ -45,7 +78,8 @@ const AlertPage = () => {
             <div className="container-full">
                 <b>Info alert</b>
                 <Alert
-                    id='3'
+                    id='6'
+                    option='secondary'
                     type='info'
                     autoDismiss='0'
                     text='Info Alert'
@@ -55,7 +89,8 @@ const AlertPage = () => {
             <div className="container-full">
                 <b>Info alert & autoDismiss 5 seconds</b>
                 <Alert
-                    id='4'
+                    id='7'
+                    option='secondary'
                     type='info'
                     autoDismiss='5000'
                     text='Info Alert'
@@ -65,7 +100,8 @@ const AlertPage = () => {
             <div className="container-full">
                 <b>Danger alert</b>
                 <Alert
-                    id='5'
+                    id='8'
+                    option='secondary'
                     type='danger'
                     text='Danger Alert'
                     autoDismiss='0'
@@ -75,7 +111,8 @@ const AlertPage = () => {
             <div className="container-full">
                 <b>Danger alert & autoDismiss 10 seconds</b>
                 <Alert
-                    id='6'
+                    id='9'
+                    option='secondary'
                     type='danger'
                     text='Danger Alert'
                     autoDismiss='10000'
