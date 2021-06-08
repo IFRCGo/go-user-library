d358fafec206e75227b45ebd16945ad47125bb07
Oliver Westbury <owestbury@gmail.com>
2021-06-08 09:48:49 +0100

    fixed loading style

--- src/views/ui-components/tables/index.js
+++ src/views/ui-components/tables/index.js
@@ -4,7 +4,9 @@ import ReactDOMServer from "react-dom/server";
 
 import Tables from "./../../../components/tables";
 import Code from "./../../../hoc/source-code";
+import DisplayTable, {SortHeader, FilterHeader} from "../../../components/tables";
 import Alert from "../alert/alert";
+import {SFPComponent} from '../../../utils/extendables';
 
 const htmlString = ReactDOMServer.renderToStaticMarkup(
     <div className="tables__block">
@@ -21,6 +23,179 @@ const htmlString = ReactDOMServer.renderToStaticMarkup(
     </span>
     </div>
 );
+
+const table = {
+    table: {
+        page: 1,
+        sort: {
+            field: '',
+            direction: 'asc'
+        },
+        filters: {
+            startDateInterval: 'all',
+            type: 'all'
+        }
+    }
+};
+
+
+const headings = [
+    {
+        id: 'id',
+        label: 'Label'
+    },
+    {
+        id: 'name',
+        label:
+            <SortHeader
+                id='name'
+                title='Label'
+                sort={table.sort}
+                onClick='id'
+            />
+    }
+];
+
+const rows = [
+    {
+        id: '1',
+        name: '2'
+    },
+    {
+        id: '3',
+        name: '4'
+    }
+];
+
+const headings2 = [
+    {
+        id: 'date',
+        label: 'Start Date'
+    },
+    {
+        id: 'appeal_type',
+        label:
+            <SortHeader
+                id='name'
+                title='Appeal Type'
+                sort={table.sort}
+                onClick='id'
+            />
+    },
+    {
+        id: 'appeal_code',
+        label: 'Appeal Code'
+    },
+    {
+        id: 'operations',
+        label: 'Active Operations'
+    },
+    {
+        id: 'disaster',
+        label: 'Disaster Type'
+    },
+    {
+        id: 'requirements',
+        label: 'Funding Requirements'
+    },
+    {
+        id: 'coverage',
+        label: 'Funding Coverage'
+    },
+];
+
+const rows2 = [
+    {
+        date: '2018-09-18',
+        appeal_type: 'DREF',
+        appeal_code: 'MDRUA010',
+        operations: 'Venezuela: Conflict',
+        disaster: 'Epidemic',
+        requirements: '208,367 CHF',
+        coverage: '80%'
+    },
+    {
+        date: '2018-09-18',
+        appeal_type: 'DREF',
+        appeal_code: 'MDRUA010',
+        operations: 'Venezuela: Conflict',
+        disaster: 'Epidemic',
+        requirements: '208,367 CHF',
+        coverage: '80%'
+    }
+];
+
+
+
+const headings3 = [
+    {
+        id: 'national_society',
+        label: 'National Society'
+    },
+    {
+        id: 'start_end_dates',
+        label:
+            <SortHeader
+                id='start_end_dates'
+                title='Start-End Dates'
+                sort={table.sort}
+                onClick='id'
+            />
+    },
+    {
+        id: 'appeal_code',
+        label: 'Appeal Code'
+    },
+    {
+        id: 'project_sector',
+        label: 'Project Sector'
+    },
+    {
+        id: 'total_budget',
+        label: 'Total Budget'
+    },
+    {
+        id: 'programme_type',
+        label: 'Programme Type'
+    },
+    {
+        id: 'disaster_type',
+        label: 'Disaster Type'
+    },
+    {
+        id: 'people_target',
+        label: 'People Target'
+    },
+    {
+        id: 'people_reached',
+        label: 'People Reached'
+    }
+];
+
+const rows3 = [
+    {
+        national_society: 'British RC',
+        start_end_dates: '2020-06-01 - 2020-08-30',
+        appeal_code: 'Covid-19 Response',
+        project_sector:'Health',
+        total_budget: '30,000 CHF',
+        programme_type: 'Bilateral',
+        disaster_type: 'Epidemic,',
+        people_target: '1,000,000',
+        people_reached: '1,000,000',
+    },
+    {
+        national_society: 'British RC',
+        start_end_dates: '2020-06-01 - 2020-08-30',
+        appeal_code: 'Covid-19 Response',
+        project_sector:'Health',
+        total_budget: '30,000 CHF',
+        programme_type: 'Bilateral',
+        disaster_type: 'Epidemic,',
+        people_target: '1,000,000',
+        people_reached: '1,000,000',
+    }
+];
 
 const TablesPage = () => {
     return (
@@ -46,51 +221,36 @@ const TablesPage = () => {
                     </TabList>
 
                     <TabPanel>
-                        <b>Table / Label 56px</b>
-                        <Alert
-                            id='1'
-                            option='primary'
-                            type='warning'
-                            text='Warning Alert'
-                            autoDismiss='0'
+                        <b>Table / Label & Body 56px</b>
+
+                        <DisplayTable
+                            headings={headings}
+                            rows={rows}
                         />
 
-                        <b>Table / Body 56px</b>
-                        <Alert
-                            id='1'
-                            option='primary'
-                            type='warning'
-                            text='Warning Alert'
-                            autoDismiss='0'
+                        <b>Table - 56px</b>
+                        <DisplayTable
+                            headings={headings2}
+                            rows={rows2}
                         />
 
-                        <b>Table / Body 72px</b>
-                        <Alert
-                            id='1'
-                            option='primary'
-                            type='warning'
-                            text='Warning Alert'
-                            autoDismiss='0'
+                        <b>Table collapse - 56px</b>
+                        <DisplayTable
+                            headings={headings3}
+                            rows={rows3}
                         />
 
-                        <b>TABLE EXAMPLE</b>
-                        <Alert
-                            id='1'
-                            option='primary'
-                            type='warning'
-                            text='Warning Alert'
-                            autoDismiss='0'
-                        />
                     </TabPanel>
                     <TabPanel>
                         <Code source={`${htmlString}`}/>
                     </TabPanel>
                 </Tabs>
             </div>
-          <div className="fold__body">
-            <h2 className="fold__title margin-reset">COMMENTS</h2>
-            <p>Table elements are organised in groups to facilitate the creation of patterns. Table can be single-lined (56px) or double-lined (72px).</p>
-          </div>
+            <div className="fold__body">
+                <h2 className="fold__title margin-reset">COMMENTS</h2>
+                <p>Table elements are organised in groups to facilitate the creation of patterns. Table can be
+                    single-lined (56px) or double-lined (72px).</p>
+            </div>
         </React.Fragment>
     );
 };
