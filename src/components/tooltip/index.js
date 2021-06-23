1fad641ef6e1e2b2c89103ff7d2542a6ab7b3bb2
Oliver Westbury <owestbury@gmail.com>
2021-06-23 13:18:36 +0100

    fixed tooltips

--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -2,46 +2,80 @@ import React from "react";
 import InfoIcon from "./info-icon";
 import ReactTooltip from "react-tooltip";
 
-const Tooltip = ({ title, description }) => {
-  return (
-    <React.Fragment>
-      <a
-        data-tip
-        data-for={title ? title : description}
-        data-event="click focus"
-      >
-        <InfoIcon className="tooltip" />
-      </a>
-      <ReactTooltip
-        className="tooltip"
-        globalEventOff="click"
-        id={title ? title : description}
-        aria-haspopup="true"
-      >
-        {title ? <header className="tooltip__header">{title}</header> : null}
-        {description ? (
-          <span
-            className="tooltip__text"
-            dangerouslySetInnerHTML={{ __html: description }}
-          ></span>
-        ) : null}
-      </ReactTooltip>
-    </React.Fragment>
-  );
+const Tooltip = ({type, title, description}) => {
+    console.log(type);
+    return (
+        <React.Fragment>
+            {type === 'popup' ? (
+                <a
+                    data-tip
+                    data-for={title ? title : description}
+                    data-event="click focus"
+                >
+                    POPUPs
+                    <InfoIcon className="popup"/>
+                </a>
+            ) : null}
+            {type === 'popup' ? (
+                <ReactTooltip
+                    className="tooltip"
+                    globalEventOff="click"
+                    id={title ? title : description}
+                    aria-haspopup="true"
+                >
+                    {title ? <header className="tooltip__header">{title}</header> : null}
+                    {description ? (
+                        <span
+                            className="tooltip__text"
+                            dangerouslySetInnerHTML={{__html: description}}
+                        ></span>
+                    ) : null}
+                </ReactTooltip>
+            ) : null}
+
+
+            {type != 'popup' ? (
+                <a
+                    data-tip
+                    data-for={title ? title : description}
+                    data-event="click focus"
+                >
+                    <InfoIcon className="tooltip"/>
+                </a>
+            ) : null}
+            {type != 'popup' ? (
+                <ReactTooltip
+                    className="tooltip"
+                    globalEventOff="click"
+                    id={title ? title : description}
+                    aria-haspopup="true"
+                >
+                    {title ? <header className="tooltip__header">{title}</header> : null}
+                    {description ? (
+                        <span
+                            className="tooltip__text"
+                            dangerouslySetInnerHTML={{__html: description}}
+                        ></span>
+                    ) : null}
+                </ReactTooltip>
+            ) : null}
+
+        </React.Fragment>
+    );
 };
 
-export const HoverTooltip = ({ title, description, id }) => {
-  return (
-    <ReactTooltip className="tooltip" id={id}>
-      {title ? <header className="tooltip__header">{title}</header> : null}
-      {description ? (
-        <span
-          className="tooltip__text"
-          dangerouslySetInnerHTML={{ __html: description }}
-        ></span>
-      ) : null}
-    </ReactTooltip>
-  );
+export const HoverTooltip = ({title, description, id}) => {
+    return (
+        <ReactTooltip className="tooltip" id={id}>
+            {title ? <header className="tooltip__header">{title}</header> : null}
+            {description ? (
+                <span
+                    className="tooltip__text"
+                    dangerouslySetInnerHTML={{__html: description}}
+                ></span>
+            ) : null}
+        </ReactTooltip>
+    );
 };
 
 export default Tooltip;
