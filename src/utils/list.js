4d8278ad8b2498f761f6b245346b16cfd85c5f08
Szabo, Zoltan <szabozoltan69@gmail.com>
2021-09-14 20:27:14 +0200

    about link

--- src/utils/list.js
+++ src/utils/list.js
@@ -229,7 +229,7 @@ export const ListData = {
                     },
                     {
                         name: 'Other Resources',
-                        link: '/about'
+                        link: 'https://go.ifrc.org/about'
                     },
                 ]
             },
