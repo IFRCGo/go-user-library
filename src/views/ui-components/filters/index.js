0281bced5bfb8c71ab4c48c745c7a4520546f93e
Szabo, Zoltan <szabozoltan69@gmail.com>
2021-09-29 11:24:55 +0200

    Removing a not used module

--- src/views/ui-components/filters/index.js
+++ src/views/ui-components/filters/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
 import ReactDOMServer from "react-dom/server";
 import Code from "./../../../hoc/source-code";
-import Alert from "../alert/alert";
 import Dropdown from "react-bootstrap/Dropdown";
 import Header from "../../../hoc/header";
 import {ListData} from "../../../utils/list";
